3baf9f2627093f58658126dc5c946825dfdb78fb
Ivan <ivan@ostrovok.ru>
2011-12-12 16:09:57 +0400

    Add xhtml2pdf converter

--- webodt_sample/views.py
+++ webodt_sample/views.py
@@ -10,6 +10,15 @@ def test_pdf(request):
         context_instance=RequestContext(request)
     )
 
+
+def test_pdf_from_html(request):
+    context = {'username': 'John Doe', 'balance': 10.01}
+    return render_to_response('sample.html',
+        dictionary=context, format='pdf', filename='test1.pdf',
+        context_instance=RequestContext(request), cache=None,
+    )
+
+
 def test_iterator(request):
     iterator = request.GET.get('iterator') == 'true'
     context = {'username': 'John Doe', 'balance': 10.01}
